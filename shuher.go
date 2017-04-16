221d85bc5e32270acf09de4ae3914246113be324
malashin <malashinanton@gmail.com>
2017-04-17 01:03:12 +1000

    Testing

--- shuher.go
+++ shuher.go
@@ -2,31 +2,68 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/jlaffaye/ftp"
 )
 
+type entry struct {
+	Name string
+	Type entryType
+	Size uint64
+	Time time.Time
+	Path string
+}
+
+type entryType int
+
+const (
+	entryTypeFile entryType = iota
+	entryTypeFolder
+	entryTypeLink
+)
+
 // Global variables.
 // Ftp server address with port.
-var addr = ""
-var user = ""
-var password = ""
+// var addr = ""
+// var user = ""
+// var password = ""
+
+func parseFolder(connection *ftp.ServerConn, currentPath string) {
+	entries, err := connection.List("")
+	if err != nil {
+		panic(err)
+	}
+	for _, element := range entries {
+		if element.Type == 1 {
+			fmt.Println(element)
+		}
+	}
+}
 
 func main() {
-	// Create client object with default config.
+	// Initialize the connection to the specified ftp server address.
 	connection, err := ftp.Dial(addr)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Connection to " + addr + " is initialized")
 	// Properly close the connection on exit.
 	defer connection.Quit()
+
 	// Authenticate the client with specified user and password.
 	err = connection.Login(user, password)
 	if err != nil {
 		panic(err)
 	}
-	entries, err := connection.List("/")
-	for _, element := range entries {
-		fmt.Println(element.Name)
+	fmt.Println("Logged in as " + user)
+	var currentPath = "/"
+
+	err = connection.ChangeDir("/AMEDIATEKA")
+	if err != nil {
+		panic(err)
 	}
+	currentPath = "/AMEDIATEKA"
+
+	parseFolder(connection, currentPath)
 }
