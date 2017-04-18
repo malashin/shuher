8682032b0851fe8114875381c450b55cf4afb3a1
malashin <malashinanton@gmail.com>
2017-04-18 15:25:41 +1000

    Refactoring

--- shuher.go
+++ shuher.go
@@ -15,58 +15,107 @@ import (
 	"github.com/jlaffaye/ftp"
 )
 
-func dial(addr string) *ftp.ServerConn {
+type ftpConn struct {
+	conn      *ftp.ServerConn
+	logger    ILogger
+	connected bool
+}
+
+func newFtpConn() *ftpConn {
+	return &ftpConn{}
+}
+
+func (f *ftpConn) setLogger(l ILogger) {
+	f.logger = l
+}
+
+func (f *ftpConn) Log(text ...interface{}) {
+	if f.logger != nil {
+		f.logger.Log(text...)
+	}
+}
+
+func (f *ftpConn) dial(addr string) {
 	conn, err := ftp.Dial(addr)
 	if err != nil {
-		log.Fatalln(err)
+		log.Panicln(err)
 	}
-	consoleLog("Connected to " + addr)
-	return conn
+	f.conn = conn
+	f.connected = true
+	f.Log("Connected to " + addr)
 }
 
-func quit(conn *ftp.ServerConn) {
-	conn.Quit()
-	consoleLog("Connection closed")
-}
-
-func login(conn *ftp.ServerConn, user string, pass string) {
-	err := conn.Login(user, pass)
-	if err != nil {
-		log.Fatalln(err)
+func (f *ftpConn) quit() {
+	if !f.connected {
+		return
 	}
-	consoleLog("Logged in as " + user)
+	f.conn.Quit()
+	f.connected = false
+	f.Log("Connection closed")
 }
 
-func cwd(conn *ftp.ServerConn) string {
-	cwd, err := conn.CurrentDir()
+func (f *ftpConn) login(user string, pass string) {
+	if !f.connected {
+		f.Log("ERROR: ftpConn.login() No FTP connection")
+		return
+	}
+	err := f.conn.Login(user, pass)
 	if err != nil {
-		log.Fatalln(err)
+		log.Panicln(err)
+	}
+	f.Log("Logged in as " + user)
+}
+
+func (f *ftpConn) cwd() string {
+	if !f.connected {
+		f.Log("ERROR: ftpConn.cwd() No FTP connection")
+		return ""
+	}
+	cwd, err := f.conn.CurrentDir()
+	if err != nil {
+		log.Panicln(err)
 	}
 	return cwd
 }
 
-func cd(conn *ftp.ServerConn, path string) {
-	err := conn.ChangeDir(path)
+func (f *ftpConn) cd(path string) {
+	if !f.connected {
+		f.Log("ERROR: ftpConn.cd() No FTP connection")
+		return
+	}
+	err := f.conn.ChangeDir(path)
 	if err != nil {
-		log.Fatalln(err)
+		log.Panicln(err)
 	}
 }
 
-func cdup(conn *ftp.ServerConn) {
-	conn.ChangeDirToParent()
+func (f *ftpConn) cdup() {
+	if !f.connected {
+		f.Log("ERROR: ftpConn.cdup() No FTP connection")
+		return
+	}
+	f.conn.ChangeDirToParent()
 }
 
-func ls(conn *ftp.ServerConn, path string) (entries []*ftp.Entry) {
-	entries, err := conn.List(path)
+func (f *ftpConn) ls(path string) (entries []*ftp.Entry) {
+	if !f.connected {
+		f.Log("ERROR: ftpConn.ls() No FTP connection")
+		return
+	}
+	entries, err := f.conn.List(path)
 	if err != nil {
-		log.Fatalln(err)
+		log.Panicln(err)
 	}
 	return entries
 }
 
-func walk(conn *ftp.ServerConn) {
-	entries := ls(conn, "")
-	cwd := cwd(conn)
+func (f *ftpConn) walk(fl map[string]fileEntry) {
+	if !f.connected {
+		f.Log("ERROR: ftpConn.walk() No FTP connection")
+		return
+	}
+	entries := f.ls("")
+	cwd := f.cwd()
 	newLine := pad(cwd, len(lastLine))
 	fmt.Print(newLine + "\r")
 	lastLine = cwd
@@ -75,29 +124,29 @@ func walk(conn *ftp.ServerConn) {
 		case ftp.EntryTypeFile:
 			if acceptFileName(element.Name) {
 				key := cwd + "/" + element.Name
-				entry, fileExists := fileList[key]
+				entry, fileExists := fl[key]
 				if fileExists {
 					// Old file with new date
 					if !entry.Time.Equal(element.Time) {
-						consoleLog("~ " + truncPad(key, 40, 'l') + " datetime changed")
-						fileList[key] = newFileEntry(element)
+						f.Log("~ " + truncPad(key, 40, 'l') + " datetime changed")
+						fl[key] = newFileEntry(element)
 					} else if entry.Size != element.Size {
-						consoleLog("~ " + truncPad(key, 40, 'l') + " size changed")
-						fileList[key] = newFileEntry(element)
+						f.Log("~ " + truncPad(key, 40, 'l') + " size changed")
+						fl[key] = newFileEntry(element)
 					} else {
 						entry.Found = true
-						fileList[key] = entry
+						fl[key] = entry
 					}
 				} else {
 					// New file
-					consoleLog("+ " + truncPad(key, 40, 'l') + " new file")
-					fileList[key] = newFileEntry(element)
+					f.Log("+ " + truncPad(key, 40, 'l') + " new file")
+					fl[key] = newFileEntry(element)
 				}
 			}
 		case ftp.EntryTypeFolder:
-			cd(conn, element.Name)
-			walk(conn)
-			cdup(conn)
+			f.cd(element.Name)
+			f.walk(fl)
+			f.cdup()
 		}
 	}
 }
@@ -132,7 +181,6 @@ func acceptFileName(fileName string) bool {
 func newFileEntry(entry *ftp.Entry) fileEntry {
 	file := fileEntry{}
 	file.Name = entry.Name
-	file.Type = entryType(entry.Type)
 	file.Size = entry.Size
 	file.Time = entry.Time
 	file.Found = true
@@ -141,29 +189,37 @@ func newFileEntry(entry *ftp.Entry) fileEntry {
 
 type fileEntry struct {
 	Name  string
-	Type  entryType
 	Size  uint64
 	Time  time.Time
 	Found bool
 }
 
 func (fe *fileEntry) pack() string {
-	return fe.Name + "?|" + fmt.Sprintf("%v", fe.Type) + "?|" + fmt.Sprintf("%v", fe.Size) + "?|" + fe.Time.String()
+	return fe.Name + "?|" + fmt.Sprintf("%v", fe.Size) + "?|" + fe.Time.String()
 }
 
-type entryType int
+type tFileList struct {
+	file   map[string]fileEntry
+	logger ILogger
+}
 
-const (
-	entryTypeFile entryType = iota
-	entryTypeFolder
-	entryTypeLink
-)
+func newFileList() *tFileList {
+	return &tFileList{file: map[string]fileEntry{}}
+}
 
-type tFileList map[string]fileEntry
+func (fl *tFileList) setLogger(l ILogger) {
+	fl.logger = l
+}
+
+func (fl *tFileList) Log(text ...interface{}) {
+	if fl.logger != nil {
+		fl.logger.Log(text...)
+	}
+}
 
 func (fl *tFileList) pack() string {
 	output := []string{}
-	for key, value := range *fl {
+	for key, value := range fl.file {
 		output = append(output, "?{"+key+"?}"+value.pack()+"\n")
 	}
 	sort.Strings(output)
@@ -171,10 +227,10 @@ func (fl *tFileList) pack() string {
 }
 
 func (fl *tFileList) clean() {
-	for key, value := range *fl {
+	for key, value := range fl.file {
 		if !value.Found {
-			delete(*fl, key)
-			consoleLog("- " + truncPad(key, 40, 'l') + " deleted")
+			delete(fl.file, key)
+			fl.Log("- " + truncPad(key, 40, 'l') + " deleted")
 		}
 	}
 }
@@ -186,80 +242,87 @@ func (fl tFileList) String() string {
 func (fl *tFileList) save(filepath string) {
 	file, err := os.Create(filepath)
 	if err != nil {
-		log.Fatalln(err)
+		log.Panicln(err)
 	}
 	defer file.Close()
 
 	_, err = io.Copy(file, strings.NewReader(fl.pack()))
 	if err != nil {
-		log.Fatalln(err)
+		log.Panicln(err)
 	}
 }
 
 func (fl *tFileList) load(filepath string) {
-	consoleLog("Loading \"" + filepath + "\"...")
+	fl.Log("Loading \"" + filepath + "\"...")
 	file, err := os.Open(filepath)
 	if err != nil {
-		consoleLog("\"" + filepath + "\" not found")
+		fl.Log("\"" + filepath + "\" not found")
 		return
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		key, entry := parseLine(scanner.Text())
+		key, entry := fl.parseLine(scanner.Text())
 		if key != "" {
-			fileList[key] = entry
+			fl.file[key] = entry
 		}
 	}
 
 	err = scanner.Err()
 	if err != nil {
-		log.Fatalln(err)
+		log.Panicln(err)
 	}
 }
 
-func createLog() (*os.File, *log.Logger) {
-	file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return file, log.New(file, "", log.LstdFlags)
-}
-
-func closeLog(file *os.File) {
-	defer file.Close()
-}
-
-func consoleLog(text ...interface{}) {
-	log.Println(text...)
-	logger.Print(text...)
-}
-
-func parseLine(line string) (string, fileEntry) {
-	// "?{/AMEDIATEKA/ANIMALS_2/SER_05620.mxf?}SER_05620.mxf?|0?|13114515508?|2017-03-17 14:39:39 +0000 UTC"
+func (fl *tFileList) parseLine(line string) (string, fileEntry) {
+	// "?{/AMEDIATEKA/ANIMALS_2/SER_05620.mxf?}SER_05620.mxf?|13114515508?|2017-03-17 14:39:39 +0000 UTC"
 	if !regExpLine.MatchString(line) {
-		consoleLog("ERROR: Wrong input in file list (" + line + ")")
+		fl.Log("ERROR: Wrong input in file list (" + line + ")")
 		return "", fileEntry{}
 	}
 	matches := regExpLine.FindStringSubmatch(line)
 	key := matches[1]
 	entry := fileEntry{}
 	entry.Name = matches[2]
-	entryFileType, err := strconv.Atoi(matches[3])
-	if err != nil {
-		consoleLog(err)
-		return "", fileEntry{}
-	}
-	entry.Type = entryType(entryFileType)
-	entrySize, err := strconv.Atoi(matches[4])
+	entrySize, err := strconv.Atoi(matches[3])
 	entry.Size = uint64(entrySize)
-	entry.Time, err = time.Parse("2006-01-02 15:04:05 +0000 UTC", matches[5])
+	entry.Time, err = time.Parse("2006-01-02 15:04:05 +0000 UTC", matches[4])
 	if err != nil {
-		consoleLog(err)
+		fl.Log(err)
 		return "", fileEntry{}
 	}
 	return key, entry
+}
+
+// ILogger ...
+type ILogger interface {
+	Log(text ...interface{})
+}
+
+type logger struct {
+	file   *os.File
+	logger *log.Logger
+}
+
+func newLogger() *logger {
+	l := &logger{}
+	file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	if err != nil {
+		log.Panicln(err)
+	}
+	l.file = file
+	l.logger = log.New(file, "", log.LstdFlags)
+	return l
+}
+
+func (l *logger) close() {
+	l.file.Close()
+}
+
+func (l *logger) Log(text ...interface{}) {
+	log.Println(text...)
+	l.logger.Print(text...)
 }
 
 // Global variables are set in private file.
@@ -268,41 +331,43 @@ func parseLine(line string) (string, fileEntry) {
 // var user = ""
 // var pass = ""
 
-var fileList = tFileList{}
-var regExpLine = regexp.MustCompile(`\?\{(.*)\?\}(.*)\?\|(\d)\?\|(\d+)\?\|(.*)$`)
+var regExpLine = regexp.MustCompile(`\?\{(.*)\?\}(.*)\?\|(\d+)\?\|(.*)$`)
 var logFilePath = "shuher.log"
+var fileListPath = "shuherFileList.txt"
 var watcherRootPath = "/AMEDIATEKA"
 var fileMask = regexp.MustCompile(`^.*\.mxf$`)
-var logger *log.Logger
 var lastLine string
 var sleepTime = 10 * time.Minute
 
 func main() {
-	// Open log file.
-	var logFile *os.File
-	logFile, logger = createLog()
-	defer closeLog(logFile)
+	// Create objects.
+	logger := newLogger()
+	defer logger.close()
+	ftpConn := newFtpConn()
+	ftpConn.setLogger(logger)
+	fileList := newFileList()
+	fileList.setLogger(logger)
 	// Load file list.
-	fileList.load("shuherFileList.txt")
+	fileList.load(fileListPath)
+	// Properly close the connection on exit.
+	defer ftpConn.quit()
 
 	for {
 		// Initialize the connection to the specified ftp server address.
-		conn := dial(addr)
-		// Properly close the connection on exit.
-		defer quit(conn)
+		ftpConn.dial(addr)
 		// Authenticate the client with specified user and password.
-		login(conn, user, pass)
+		ftpConn.login(user, pass)
 		// Change directory watcherRootPath.
-		cd(conn, watcherRootPath)
+		ftpConn.cd(watcherRootPath)
 		// Walk the directory tree.
-		consoleLog("Looking for new files...")
-		walk(conn)
+		logger.Log("Looking for new files...")
+		ftpConn.walk(fileList.file)
 		// Remove deleted files from the fileList.
 		fileList.clean()
 		// Save new fileList.
-		fileList.save("shuherFileList.txt")
+		fileList.save(fileListPath)
 		// Terminate the FTP connection.
-		quit(conn)
+		ftpConn.quit()
 		// Wait for sleepTime before checking again.
 		time.Sleep(sleepTime)
 	}
